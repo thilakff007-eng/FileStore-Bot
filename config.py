a5ea00853e6f68a87f1938746c18a2c0effc42b6
thilakff007-eng <thilakff007@gmail.com>
2026-03-22 19:57:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -41,7 +41,7 @@ START_PIC = os.environ.get("START_PIC", "https://telegra.ph/file/ec17880d61180d3
 FORCE_PIC = os.environ.get("FORCE_PIC", "https://telegra.ph/file/e292b12890b8b4b9dcbd1.jpg")
 
 #--------------------------------------------
-SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "https://arolinks.com/bH1T71")
+SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "https://arolinks.com")
 SHORTLINK_API = os.environ.get("SHORTLINK_API", "4134296f6ba51789996b73ae880bf798671b6d9b")
 TUT_VID = os.environ.get("TUT_VID","https://t.me/hwdownload/3")
 SHORT_MSG = "<b>⌯ Here is Your Download Link, Must Watch Tutorial Before Clicking On Download...</b>"
