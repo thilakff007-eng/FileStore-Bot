87fea2509156f8cca025fc9ea4cc3ddc964e93cb
thilakff007-eng <thilakff007@gmail.com>
2026-03-23 18:02:44 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -113,7 +113,7 @@ class Bot(Client):
         await web.TCPSite(app, "0.0.0.0", PORT).start()
 
 
-        try: await self.send_message(OWNER_ID, text = f"<b><blockquote> Bᴏᴛ Rᴇsᴛᴀʀᴛᴇᴅ by @Codeflix_Bots</blockquote></b>")
+        try: await self.send_message(OWNER_ID, text = f"<b><blockquote> Bᴏᴛ Rᴇsᴛᴀʀᴛᴇᴅ by @Animeworld_07</blockquote></b>")
         except: pass
 
     async def stop(self, *args):
@@ -124,7 +124,7 @@ class Bot(Client):
         """Run the bot."""
         loop = asyncio.get_event_loop()
         loop.run_until_complete(self.start())
-        self.LOGGER(__name__).info("Bot is now running. Thanks to @rohit_1888")
+        self.LOGGER(__name__).info("Bot is now running. Thanks to @thilakff")
         try:
             loop.run_forever()
         except KeyboardInterrupt:
